62a550128a65f46e79053a862494bd9bd6d9bd3f
Yidong Li <liyidong@xiaohongshu.com>
2025-03-06 11:54:04 +0800

    feat(media-resource): update DemoLynxMediaResourceFetcher to support local assets

--- android/KotlinEmptyProject/app/src/main/java/com/lynx/kotlinemptyproject/DemoLynxMediaResourceFetcher.kt
+++ android/KotlinEmptyProject/app/src/main/java/com/lynx/kotlinemptyproject/DemoLynxMediaResourceFetcher.kt
@@ -6,6 +6,7 @@ import com.lynx.tasm.resourceprovider.LynxResourceRequest
 import com.lynx.tasm.resourceprovider.LynxResourceResponse
 import com.lynx.tasm.resourceprovider.generic.LynxGenericResourceFetcher
 import com.lynx.tasm.resourceprovider.media.LynxMediaResourceFetcher
+import com.lynx.tasm.resourceprovider.media.OptionalBool
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.io.ByteArrayOutputStream
@@ -13,53 +14,15 @@ import java.io.Closeable
 import java.io.IOException
 
 class DemoLynxMediaResourceFetcher(context: Context): LynxMediaResourceFetcher() {
-    private val client = OkHttpClient()
 
-    private var mContext: Context = context.applicationContext
-
-    override fun fetchImage(
-        request: LynxResourceRequest,
-        callback: LynxResourceCallback<Closeable>
-    ) {
-        Thread {
-            try {
-                val uri = request.url
-                if (uri.startsWith("http")) {
-                    val request = Request.Builder()
-                        .url(uri)
-                        .build()
-
-                    client.newCall(request).execute().use { response ->
-                        if (!response.isSuccessful) throw IOException("Unexpected code $response")
-
-                        for ((name, value) in response.headers) {
-                            println("$name: $value")
-                        }
-
-                        callback.onResponse(LynxResourceResponse.onSuccess(response.body!!.byteStream()))
-                    }
-                } else {
-                    mContext.assets.open(uri).use { inputStream ->
-                        ByteArrayOutputStream().use { byteArrayOutputStream ->
-                            val buffer = ByteArray(1024)
-                            var length: Int
-                            while ((inputStream.read(buffer).also { length = it }) != -1) {
-                                byteArrayOutputStream.write(buffer, 0, length)
-                            }
-                            callback.onResponse(LynxResourceResponse.onSuccess(byteArrayOutputStream))
-                        }
-                    }
-                }
-            } catch (e: IOException) {
-//                callback.onResponse(LynxResourceResponse.onFailed(Throwable(e.message)) as LynxResourceResponse<String>?)
-            }
-        }.start()
+    override fun isLocalResource(url: String): OptionalBool {
+        return if (url.startsWith("/")) { OptionalBool.TRUE } else {
+            OptionalBool.FALSE
+        }
     }
 
     override fun shouldRedirectUrl(request: LynxResourceRequest): String {
-        return request.url
+        return if (request.url.startsWith("/")) { "asset://" + request.url } else { request.url }
     }
-//    override fun fetchResourcePath(p0: LynxResourceRequest?, callback: LynxResourceCallback<String>) {
-//        callback.onResponse(LynxResourceResponse.onFailed(Throwable("fetchResourcePath not supported.")) as LynxResourceResponse<String>?);
-//    }
+
 }
--- android/KotlinEmptyProject/app/src/main/java/com/lynx/kotlinemptyproject/MainActivity.kt
+++ android/KotlinEmptyProject/app/src/main/java/com/lynx/kotlinemptyproject/MainActivity.kt
@@ -7,6 +7,7 @@ import com.lynx.tasm.LynxBooleanOption
 import com.lynx.tasm.LynxView
 import com.lynx.tasm.LynxViewBuilder
 import com.lynx.tasm.TemplateData
+// import com.lynx.tasm.utils.DisplayMetricsHolder
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +16,10 @@ class MainActivity : Activity() {
         lynxView.updateGlobalProps(TemplateData.fromMap(mapOf("platform" to "android")))
         setContentView(lynxView)
 
-//        val uri = "main.lynx.bundle";
+        val uri = "main.lynx.bundle";
         // Replace it with local lynx bundle
-         val uri = "http://10.21.27.80:3000/Swiper.lynx.bundle";
+//         val uri = "http://10.21.27.80:3000/main.lynx.bundle?fullscreen=true";
+//        val uri = "http://10.21.27.80:3000/base.lynx.bundle"
         lynxView.renderTemplateUrl(uri, "")
 
         // open switch page
@@ -26,8 +28,8 @@ class MainActivity : Activity() {
 
     private fun buildLynxView(): LynxView {
         val viewBuilder: LynxViewBuilder = LynxViewBuilder()
-        viewBuilder.setEnableGenericResourceFetcher(LynxBooleanOption.TRUE);
         viewBuilder.setMediaResourceFetcher(DemoLynxMediaResourceFetcher(this))
+        viewBuilder.setEnableGenericResourceFetcher(LynxBooleanOption.TRUE);
         viewBuilder.setTemplateProvider(DemoTemplateProvider(this))
         return viewBuilder.build(this)
     }
