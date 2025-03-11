bbf672821d1652bbd58e7e4434e318100579517b
StanNing <usher_njj@hotmail.com>
2025-03-11 19:53:19 +0800

    Fix: both deleted main.lynx.bundle

--- android/KotlinEmptyProject/app/src/main/java/com/lynx/kotlinemptyproject/MainActivity.kt
+++ android/KotlinEmptyProject/app/src/main/java/com/lynx/kotlinemptyproject/MainActivity.kt
@@ -2,8 +2,10 @@ package com.lynx.kotlinemptyproject
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import com.lynx.tasm.LynxBooleanOption
 import com.lynx.tasm.LynxLoadMeta
+import com.lynx.tasm.LynxUpdateMeta
 import com.lynx.tasm.LynxView
 import com.lynx.tasm.LynxViewBuilder
 import com.lynx.tasm.TemplateData
@@ -15,21 +17,50 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val lynxView: LynxView = buildLynxView()
-        val builder = LynxLoadMeta.Builder()
-        builder.setInitialData(TemplateData.fromMap(mapOf()))
-//        builder.setUrl("empty.lynx.bundle")
-        builder.setUrl("204to1000.lynx.bundle")
-//        builder.setUrl("http://10.21.27.80:3000/main.lynx.bundle?fullscreen=true")
-        val meta = builder.build()
         lynxView.updateGlobalProps(TemplateData.fromMap(mapOf("platform" to "android")))
         setContentView(lynxView)
-        lynxView.loadTemplate(meta)
 
-//        val uri = "main.lynx.bundle";
-        // Replace it with local lynx bundle
+        val path = intent.getStringExtra("path")
+
+        val uri = "${path}/main.lynx.bundle"
+
+        if (path.equals("cssBtsInitData")) {
+
+            val builder = LynxLoadMeta.Builder()
+
+
+            builder.setUrl(uri)
+// builder.setBinaryData();
+// builder.setTemplateBundle();
+            builder.setInitialData(TemplateData.fromMap(mapOf("title" to "XXXXXXXXXXXXXXXXXXXXXXX")))
+
+// builder.addLoadOption();
+            val meta = builder.build()
+            lynxView.loadTemplate(meta)
+
+            Thread {
+                Thread.sleep(3000)
+
+                Log.d("aaa", "3s 后执行")
+
+                lynxView.post {
+                    val builderUpdate = LynxUpdateMeta.Builder()
+                    builderUpdate.setUpdatedData(TemplateData.fromMap(mapOf("title" to "ABCDEFGHIJKLMNOPQRSTUVWXYZ")))
+                    val metaUpdate = builderUpdate.build()
+                    lynxView.updateData(metaUpdate.updatedData)
+                    Log.d("aaa", "updateData in ${Thread.currentThread().name}")
+                }
+            }.start()
+
+
+        } else {
+
+            // Replace it with local lynx bundle
 //         val uri = "http://10.21.27.80:3000/main.lynx.bundle?fullscreen=true";
 //        val uri = "http://10.21.27.80:3000/base.lynx.bundle"
-//        lynxView.renderTemplateUrl(uri, "")
+            lynxView.renderTemplateUrl(uri, "")
+        }
+
 
         // open switch page
         // startActivity(Intent(this, SwitchActivity::class.java));
---
+++ android/KotlinEmptyProject/app/src/main/java/com/lynx/kotlinemptyproject/SplashActivity.kt
@@ -0,0 +1,47 @@
+package com.lynx.kotlinemptyproject
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.compose.foundation.gestures.Orientation
+
+class SplashActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val linearLayout = LinearLayout(this)
+        linearLayout.orientation = LinearLayout.VERTICAL
+        linearLayout.gravity = Gravity.CENTER
+        val layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
+        )
+        layoutParams.gravity = Gravity.CENTER
+
+        linearLayout.addView(buildBtn("204"))
+        linearLayout.addView(buildBtn("204to1000"))
+        linearLayout.addView(buildBtn("images"))
+        linearLayout.addView(buildBtn("cssInJs"))
+        linearLayout.addView(buildBtn("css"))
+        linearLayout.addView(buildBtn("cssBst"))
+        linearLayout.addView(buildBtn("cssBtsInitData"))
+
+        setContentView(linearLayout, layoutParams)
+    }
+
+     fun buildBtn(text: String) :Button{
+         val button = Button(this)
+         button.text = "点击打开一个Lynx测试页面 - 用例：${text}"
+         button.setOnClickListener {
+             val intent = Intent(this, MainActivity::class.java)
+             intent.putExtra("path", text)
+             startActivity(intent)
+         }
+         return button
+     }
+}
